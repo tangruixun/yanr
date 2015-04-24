c9c0d582671d07ac14c491aed6fae3efc9fc01e8
tangruixun <tangruixun@gmail.com>
2015-04-24 16:48:57 +0800

    add findViewById in dlg class

--- src/com/trx/yanr/NewServerDialog.java
+++ src/com/trx/yanr/NewServerDialog.java
@@ -5,10 +5,14 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public final class NewServerDialog extends Dialog implements OnClickListener {
     
     Context context;
+    EditText svrAddrV, svrPortV, svrUsranemV, svrPwdV, nickNameV, emailV, orgV, faceV, xfaceV;
+    CheckBox loginReqV, sslReqV;
 
     public NewServerDialog (Context _context) {
         super (_context);
@@ -19,6 +23,18 @@ public final class NewServerDialog extends Dialog implements OnClickListener {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.newserver_dlg);
+        
+        svrAddrV = (EditText) findViewById (R.id.serveraddr);
+        svrPortV = (EditText) findViewById (R.id.serverport);
+        svrUsranemV = (EditText) findViewById (R.id.serverusername);
+        svrPwdV = (EditText) findViewById (R.id.serverpwd);
+        nickNameV = (EditText) findViewById (R.id.mynick);
+        emailV = (EditText) findViewById (R.id.myemail);
+        orgV = (EditText) findViewById (R.id.myorg);
+        faceV = (EditText) findViewById (R.id.myface);
+        xfaceV = (EditText) findViewById (R.id.myxface);
+        loginReqV = (CheckBox) findViewById (R.id.logincheckbox);
+        sslReqV = (CheckBox) findViewById (R.id.sslcheckbox);
 
         
     }
